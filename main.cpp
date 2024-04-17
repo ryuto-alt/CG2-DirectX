c96112dfa6dfeb02395388f3adca533f8b847087
ryuto11231204@gmail.com <ryuto11231204@gmail.com>
2024-04-17 17:08:21 +0900

    Complete Device

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <Windows.h>
 #include <cstdint>
+#include <string>
+#include<format>
+
+#include<d3d12.h>
+#include<dxgi1_6.h>
+#include<cassert>
+
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
+
+
 
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
@@ -14,10 +25,48 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
 }
 
 
+std::wstring ConvertString(const std::string& str) {
+	if (str.empty()) {
+		return std::wstring();
+	}
+
+	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+	if (sizeNeeded == 0) {
+		return std::wstring();
+	}
+	std::wstring result(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+	return result;
+}
+
+std::string ConvertString(const std::wstring& str) {
+	if (str.empty()) {
+		return std::string();
+	}
+
+	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+	if (sizeNeeded == 0) {
+		return std::string();
+	}
+	std::string result(sizeNeeded, 0);
+	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
+	return result;
+}
+
+
+void Log(const std::string& message) {
+	OutputDebugStringA(message.c_str());
+}
+
+
+
 
 
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+
+#pragma region Windowの生成
 
 	WNDCLASS wc{};
 	//ウィンドウプロシージャ
@@ -57,6 +106,63 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	//ウィンドウを表示する
 	ShowWindow(hwnd, SW_SHOW);
 
+#pragma endregion
+
+	//DXGIファクトリーの生成
+	IDXGIFactory7* dxgiFactory = nullptr;
+
+	HRESULT hr = CreateDXGIFactory(IID_PPV_ARGS(&dxgiFactory));
+
+	assert(SUCCEEDED(hr));
+
+
+	//使用するアダプタ用の変数　最初にnullptrを入れておく
+	IDXGIAdapter4* useAdapter = nullptr;
+
+	//いい順にアダプタを頼む
+	for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(i,
+		DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&useAdapter)) !=
+		DXGI_ERROR_NOT_FOUND; ++i) {
+		//アダプター情報を取得する
+		DXGI_ADAPTER_DESC3 adapterDisc{};
+		hr = useAdapter->GetDesc3(&adapterDisc);
+		assert(SUCCEEDED(hr));//取得できないのは一大事
+
+		//ソフトウェアアダプタでなければ採用
+		if (!(adapterDisc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE)) {
+			//採用したアダプタの情報をログに出力。wstringのほうなので注意
+			Log(ConvertString(std::format(L"Use Adapater:{}\n", adapterDisc.Description)));
+			break;
+
+		}
+		useAdapter = nullptr;
+	}
+
+	assert(useAdapter != nullptr);
+
+
+	ID3D12Device* device = nullptr;
+
+	//機能レベルとログ出力用の文字列
+	D3D_FEATURE_LEVEL featureLevels[] = {
+		D3D_FEATURE_LEVEL_12_2,D3D_FEATURE_LEVEL_12_1,D3D_FEATURE_LEVEL_12_0
+	};
+
+	const char* featureLevelStrings[] = { "12.2","12.1","12.0" };
+	//高い順に生成できるか試していく
+	for (size_t i = 0; i < _countof(featureLevels); ++i) {
+
+		hr = D3D12CreateDevice(useAdapter, featureLevels[i], IID_PPV_ARGS(&device));
+
+		if (SUCCEEDED(hr)) {
+
+			Log(std::format("FeatureLevel : {}\n", featureLevelStrings[i]));
+			break;
+		}
+	}
+
+	assert(device != nullptr);
+	Log("Complete create D3D12Device!!!\n");//初期化完了のログを出す
 
 	MSG msg{};
 
@@ -71,8 +177,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		}
 	}
 
+	std::string str0{ "STRING!!!" };
+
+	std::string str1{ std::to_string(10) };
+
 
 	OutputDebugStringA("Hello,DirectX\n");
+
+
+
 
 	return 0;
 }
